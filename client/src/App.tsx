d492fadb27e734fe5c408ba49ec06c5acefe2602
sebastien-fresse-revvity <sebastien.fresse@revvity.com>
2026-08-29 00:14:09 +0200

    feat: add fullscreen game app

---
+++ client/src/App.tsx
@@ -0,0 +1,6 @@
+// Direction « Monarchie en pyjama » : aucun chrome React ne concurrence la scène ; le jeu plein écran est l’unique expérience.
+import GameCanvas from "./components/GameCanvas";
+
+export default function App() {
+  return <GameCanvas />;
+}
